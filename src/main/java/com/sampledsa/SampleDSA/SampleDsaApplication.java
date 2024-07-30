8a8837928d31ad3f2fce4c5f632ec064ef559748
Manasa S K <manasa.sk@wissen.com>
2024-07-30 15:54:19 +0530

    initial commit

---
+++ src/main/java/com/sampledsa/SampleDSA/SampleDsaApplication.java
@@ -0,0 +1,14 @@
+package com.sampledsa.SampleDSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDsaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sampledsa/SampleDSA/SampleDsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampledsa.SampleDSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
